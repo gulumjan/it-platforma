765794c067d35de21b5685644336fb4037a2665c
gulumjan <zakirovagulumjan53@gmail.com>
2025-01-11 16:12:31 +0600

    new changing

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import scss from "./Footer.module.scss";
-import footer_img from "@/assets/footer_logo.png"
+import footer_img from "@/assets/footer_logo.png";
 import Image from "next/image";
 import { FaInstagram } from "react-icons/fa";
 import { AiOutlineYoutube } from "react-icons/ai";
@@ -16,51 +16,74 @@ const Footer: FC = () => {
     <section id={scss.footer}>
       <div className="container">
         <div className={scss.footer}>
-
-        <div className={scss.footer1}>
-          <Image src={footer_img} alt="img" />
-          <div className={scss.btn}>
-            <button>KG</button>
-            <button>RU</button>
+          <div className={scss.footer1}>
+            <Image src={footer_img} alt="img" />
+            <div className={scss.btn}>
+              <button>KG</button>
+              <button>RU</button>
+            </div>
           </div>
 
+          <div className={scss.footer2}>
+            <h3>Быстрые ссылки</h3>
+            <p>О школе</p>
+            <p>Наши курсы</p>
+            <p>О нас</p>
+          </div>
 
-        </div>
+          <div className={scss.footer3}>
+            <h3>Социальные сети</h3>
+            <div className={scss.footer_icon}>
+              <span>
+                <FaInstagram />
+              </span>
+              <span>
+                <AiOutlineYoutube />
+              </span>
+              <span>
+                <CiFacebook />
+              </span>
+            </div>
+            <div className={scss.footer_icon}>
+              <span>
+                <PiTwitterLogo />
+              </span>
+              <span>
+                <PiTiktokLogoThin />
+              </span>
+              <span>
+                <PiTelegramLogoLight />
+              </span>
+            </div>
+          </div>
 
-        <div className={scss.footer2}>
-          <h3>Быстрые ссылки</h3>
-          <p>О школе</p>
-          <p>Наши курсы</p>
-          <p>О нас</p>
-        </div>
+          <div className={scss.footer4}>
+            <h3>Контакты</h3>
+            <p>
+              <span>
+                <GoMail />
+              </span>
+              @motionwebteam@gmail.com
+            </p>
+            <p>
+              <span>
+                <IoCallOutline />
+              </span>
+              +996 559 - 69 - 26 - 26
+            </p>
+          </div>
 
-        <div className={scss.footer3}>
-          <h3>Социальные сети</h3>
-          <div className={scss.footer_icon}>
-            <span><FaInstagram />
-            </span>
-            <span><AiOutlineYoutube /></span>
-            <span><CiFacebook /></span>
-            <span><PiTwitterLogo /></span>
-            <span><PiTiktokLogoThin /></span>
-            <span><PiTelegramLogoLight /></span>
+          <div className={scss.footer5}>
+            <h3>Адреса</h3>
+            <p>
+              <span>
+                <IoLocationOutline />
+              </span>
+              Турусбекова 109 / 3
+            </p>
+            <p>Идентификационный код: 1234567890</p>
           </div>
         </div>
-
-        <div className={scss.footer4}>
-          <h3>Контакты</h3>
-          <p><span><GoMail /></span>@motionwebteam@gmail.com</p>
-          <p><span><IoCallOutline /></span>+996 559 - 69 - 26 - 26</p>
-        </div>
-
-        <div className={scss.footer5}>
-          <h3>Адреса</h3>
-          <p><span><IoLocationOutline /></span>Турусбекова 109 / 3</p>
-          <p>Идентификационный код: 1234567890</p>
-
-        </div>
-        </div>
-        
       </div>
     </section>
   );
