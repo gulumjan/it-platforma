4ca858f9c77b030a650f0e6a211834754b864091
a1dana04 <ajdanazumakmatova@gmail.com>
2025-01-15 15:31:19 +0600

    article Finish

--- src/components/pages/Home/HomePage.tsx
+++ src/components/pages/Home/HomePage.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 import Home from "@/components/pages/Home/Home";
 import SectionHome2 from "@/components/pages/Home/SectionHome2";
 import SectionHome3 from "@/components/pages/Home/SectionHome3";
-import SectionHome4 from './SectionHome4';
-import SectionHome5 from './SectionHome5';
+import SectionHome4 from "./SectionHome4";
+import SectionHome5 from "./SectionHome5";
 
 const HomePage = () => {
-    return (
-        <div>
-             <Home />
-      <SectionHome2/>
-      <SectionHome3/> 
-      <SectionHome4/>
-    <SectionHome5/>
-        </div>
-    );
+  return (
+    <div>
+      <Home />
+      <SectionHome2 />
+      <SectionHome3 />
+      <SectionHome4 />
+      <SectionHome5 />
+    </div>
+  );
 };
 
 export default HomePage;
