45f35def8ba64cfcc4c98e3db2187ca71d1a6899
a1dana04 <ajdanazumakmatova@gmail.com>
2025-02-03 18:00:34 +0600

    aid

--- src/components/pages/Paket/Paket.tsx
+++ src/components/pages/Paket/Paket.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react';
 import scss from "@/components/pages/Paket/Paket.module.scss"
 import { useGetPaketTarifQuery } from '@/redux/api/product';
--- src/components/pages/PaketPage.tsx
+++ src/components/pages/PaketPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Paket from './Paket/Paket';
+import SectionHome4 from './Home/SectionHome4';
+import SectionHome5 from './Home/SectionHome5';
 
 const PaketPage = () => {
     return (
         <div>
             <Paket/>
+            <SectionHome4/>
+            <SectionHome5/>
         </div>
     );
 };
